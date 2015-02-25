5b6c6172fef98c86301b9021a7821404d61e4b90
kr4ng <steven.m.simoni@gmail.com>
2015-02-25 11:13:13 -0800

    working on ajaxy stuff

--- app/scripts/contentscripts.js
+++ app/scripts/contentscripts.js
@@ -1,11 +1,29 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", function(){
+  console.log('this is the first part of the code');
+  var contactimagediv = document.getElementById('contactHeaderRow');
+  console.log(chrome.extension.getURL("images/macstyle1.gif"));
+  var divs = contactimagediv.getElementsByTagName('div');
+  divs[0].getElementsByTagName('img')[0].src = chrome.extension.getURL("images/macstyle1.gif");
+});
 
 window.addEventListener("load", function() {
+  console.log('this is the second part of the code');
   var app = angular.module('Schmoozee', []);
 
   var html = document.querySelector('html');
   html.setAttribute('ng-app', '');
   html.setAttribute('ng-csp', '');
+
+  //var contactimagediv = document.getElementById('contactHeaderRow');
+  //console.log('steven',contactimagediv);
+
+  //var divs = contactimagediv.getElementsByTagName('div');
+  //divs[0].getElementsByTagName('img')[0].src = "https://media.licdn.com/media/p/7/005/0ae/1ad/2dc5c21.jpg";
+  //divs[0] is what we need
+  //for (var i = 0; i < divs.length; i += 1) {
+  //  divArray.push(divs[i].innerHTML);
+  //}
 
   var viewport = document.querySelector('body');  
   viewport.setAttribute('ng-controller', 'MainController as MCtrl');
@@ -15,25 +33,23 @@ window.addEventListener("load", function() {
   $scope.data = {ptweets:[],ctweets:[],local:[],sports:[],company:[]};
   $scope.ssId;
   this.tab = 0;
-  
-  //start polling for SSID - really not a poll, just pinging the server once
-  pollingServicePost.startPolling('schmoozeessid', 'https://schmoozee.herokuapp.com/canvaslocal2', 1000, function(response){
-    $scope.ssId = response.data;
-    console.log($scope.ssId);
-    if ($scope.ssId){
-      pollingServicePost.stopPolling('schmoozeessid');
-      //stop polling
-      //start polling
-      pollingService.startPolling('schmoozeedatum', 'https://schmoozee.herokuapp.com/canvaslocal2/update', $scope.ssId, 1000, function(response){
-        $scope.data = response.data;
-        if ($scope.data.done == true){
-          pollingService.stopPolling('schmoozeedatum');
-          console.log($scope.data);
-        }
-      });     
-    }
-  });
 
+  //$scope.imageUrl = "https://media.licdn.com/media/p/7/005/0ae/1ad/2dc5c21.jpg";
+  //var contactimagediv = document.getElementById('contactHeaderRow');
+  //var divs = contactimagediv.getElementsByTagName('div');
+  //divs[0].getElementsByTagName('img')[0].src = $scope.imageUrl;
+
+  var nameDiv = document.getElementById('con2_ileinner');
+  var name = nameDiv.innerHTML;
+  var accountDiv = document.getElementById('con4_ileinner').getElementsByTagName('a')[0];
+  var account = accountDiv.innerHTML;
+  var payload = {"name":name, "account":account};
+
+  //start polling for SSID - really not a poll, just pinging the server once
+  pollingService.startPolling('schmoozeessid', payload,'https://schmoozee.herokuapp.com/chromeplugin', 10000, function(response){
+    $scope.imageUrl = response.data;
+    pollingService.stopPolling('schmoozeessid');   
+  });
   //not doing a whole lot right now
   //stop polling
   this.isTab = function(value){
@@ -49,7 +65,7 @@ window.addEventListener("load", function() {
   var myDirective = document.createElement('div');
   myDirective.setAttribute('schmoozee-sidetab', '');
   viewport.appendChild(myDirective);
-
+  //"https://media.licdn.com/media/p/7/005/0ae/1ad/2dc5c21.jpg"
   //controller and link aren't doing a whole lot right now.
   app.directive('schmoozeeSidetab', ['$sce', function($sce) {
     return {
@@ -91,11 +107,11 @@ window.addEventListener("load", function() {
         var polls = {};
 
         return {
-            startPolling: function(name, url, ssId, pollingTime, callback) {
+            startPolling: function(name, url, payload, pollingTime, callback) {
                 // Check to make sure poller doesn't already exist
                 if (!polls[name]) {
                     var poller = function() {
-                        $http.post(url, { ssId:ssId }).then(callback);
+                        $http.post(url, payload).then(callback);
                     }
                     poller();
                     polls[name] = setInterval(poller, pollingTime || defaultPollingTime);
